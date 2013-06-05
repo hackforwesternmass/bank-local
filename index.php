dd03683ea8186ac79957fa2eb7979cfeadbd1101
Kelly Dwan <kelly.dwan@gmail.com>
2013-06-05 10:00:48 -0400

    Update styles for eventual responsifying. This does break the layering, more thought needed there.

--- index.php
+++ index.php
@@ -25,51 +25,59 @@ if ( WP_DEBUG ){
 
 get_header(); ?>
 
-<div class="hand top"></div>
-<div class="dollar fixed higher"></div>
+<div class="bg image hand top"></div>
 
-<div class="sky bottom">
-	<div class="clouds-1 bottom"></div>
-	<div class="clouds-2 bottom"></div>
-	<div class="rainbow lower"></div>
-	<div class="hills mid"></div>
-</div>
+<header id="a-story" class="sky-top">
+	<div class="container">
+		<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<?php bl_callouts( $intro_id ); ?>
+	</div>
+</header>
 
+<section id="benefits-for-you" class="sky-bottom">
+	<div class="bg image clouds-1 bottom"></div>
+	<div class="bg image clouds-2 bottom"></div>
+	<div class="bg image rainbow lower"></div>
+	<div class="bg image hills mid"></div>
 
-<div class="valley bottom">
-	<div class="waves mid">
+	<div class="container">
+		<div class="image bird top"></div>
+		<h3><?php echo get_the_title( $you_id ); ?></h3>
+		<?php bl_callouts( $you_id ); ?>
+	</div>
+</section>
+
+<section id="benefits-for-community" class="valley">
+	<div class="bg image waves mid">
 		<div class="waves-1"><div></div></div>
 		<div class="waves-2"><div></div></div>
 		<div class="seamonster"></div>
 		<div class="waves-3"><div></div></div>
 	</div>
-	<div class="trees mid"></div>
-	<div class="houses mid"></div>
-</div>
+	<div class="bg image trees mid"></div>
+	<div class="bg image houses mid"></div>
 
-<div class="pig-bottom bottom"></div>
-<div class="pig top"></div>
-
-<header class="top">
-	<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-	<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-	<?php bl_callouts( $intro_id ); ?>
-</header>
-<section id="benefits-for-you" class="top">
-	<div class="bird top"></div>
-	<h3><?php echo get_the_title( $you_id ); ?></h3>
-	<?php bl_callouts( $you_id ); ?>
-</section>
-<section id="benefits-for-community" class="top">
-	<h3><?php echo get_the_title( $community_id ); ?></h3>
-	<?php bl_callouts( $community_id ); ?>
-</section>
-<section id="make-the-switch">
-	<h3><?php echo get_the_title( $join_id ); ?></h3>
-	<?php bl_callouts( $join_id ); ?>
-	<div class="bg">
-	<?php //if ( function_exists( 'bl_display_map' ) ) bl_display_map(); ?>
+	<div class="container">
+		<h3><?php echo get_the_title( $community_id ); ?></h3>
+		<?php bl_callouts( $community_id ); ?>
 	</div>
 </section>
+
+<section id="make-the-switch">
+	<div class="image pig-bottom bottom"></div>
+	<div class="image pig top"></div>
+
+	<div class="container">
+		<h3><?php echo get_the_title( $join_id ); ?></h3>
+		<?php bl_callouts( $join_id ); ?>
+	</div>
+
+	<!--<div class="bg">
+	<?php //if ( function_exists( 'bl_display_map' ) ) bl_display_map(); ?>
+	</div>-->
+</section>
+
+<div class="bg image dollar fixed higher"></div>
 
 <?php get_footer(); ?>
